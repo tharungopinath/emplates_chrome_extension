9faae453f3af57668df91426158a8df45144e087
tharun <tharunkaarthikgopinath@gmail.com>
2025-12-23 15:43:42 -0500

    fixed content.js syntax errors to recognize compose windows in Outlook

--- content.js
+++ content.js
@@ -1,14 +1,16 @@
 console.log("Email Templates Extension has started!");
 
 function checkForCompose() {
-    const gmailToolbar = document.querySelector('.btC');
-    const outlookToolbar = document.querySelector('[aria-label="Command toolbar"]');
+    const gmailToolbar = document.querySelector(".btC");
+    
+    const outlookSendButton = document.querySelector("[data.testid='ComposeSendButton']");
+    const outlookToolbar = outlookSendButton ? outlookSendButton.parentElement : null;
 
-    if (gmailToolbar) {
+    if (gmailToolbar && !document.querySelector(".myTemplates")) {
         console.log("Found gmail compose window!");
     }
 
-    if (outlookToolbar){
+    if (outlookToolbar && !document.querySelector(".myTemplates")){
         console.log("Found outlook compose window!");
     }
 }
